2df1ebfd4260d22d9d96b11cfccce49cd9a083d5
melky <melkytam17@gmail.com>
2024-11-10 22:37:28 -0300

    Implementa crud de brands na api

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,48 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BrandStoreRequest;
+use App\Http\Requests\BrandUpdateRequest;
 use App\Models\Brand;
-use Illuminate\Http\Request;
+use App\Services\BrandService;
+use Illuminate\Support\Facades\Gate;
 
 class BrandController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    public function __construct(protected BrandService $service)
+    {}
+
     public function index()
     {
-        //
+        Gate::authorize('viewAny', Brand::class);
+        $brands = $this->service->list();
+
+        return response()->json($brands);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function store(BrandStoreRequest $request)
     {
-        //
+        Gate::authorize('create', Brand::class);
+        $brand = $this->service->store($request);
+
+        return response()->json($brand);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Brand $brand)
     {
-        //
+        Gate::authorize('view', $brand);
+        return response()->json($brand);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Brand $brand)
+    public function update(BrandUpdateRequest $request, Brand $brand)
     {
-        //
+        Gate::authorize('update', $brand);
+        $brand = $this->service->update($request, $brand);
+
+        return response()->json($brand);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Brand $brand)
     {
-        //
+        Gate::authorize('delete', $brand);
+        $this->service->destroy($brand);
+
+        return response()->json(['message' => 'Brand deleted.']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\BrandController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('brands', BrandController::class)->middleware('auth:sanctum');
 
 require __DIR__.'/auth.php';
