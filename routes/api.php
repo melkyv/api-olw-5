9ec20636244cb6eeb847bcc5be1422c80abe0936
melky <melkytam17@gmail.com>
2024-12-03 19:27:12 -0300

    Carrega role do usuário logado no endpoint de usuário na api

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+    return $request->user()->load(['role']);
 });
 
 Route::apiResource('brands', BrandController::class)->middleware('auth:sanctum');
